d3ea458d1b1ef1693b56da367cf96b8ccc5cfa7b
Darlan Alves <me@darlanalv.es>
2020-08-17 00:32:10 +0200

    fix: update syntax to use core module

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { lambda, Format } from 'node-lambdas';
+import { lambda, Format } from '@node-lambdas/core';
 
 const configutation = {
   version: 1,
-  readBody: true,
   input: Format.Json,
   output: Format.Text
 };
+
 lambda(configutation, (input, output) => {
   const spaces = (Number(input.url.slice(1)) | 0) || 2;
   output.send(JSON.stringify(input.body, null, spaces));
