90a2e8f62d40352febc4b143ca5112cd1738d428
Darlan Alves <me@darlanalv.es>
2020-08-17 10:22:22 +0200

    fix: correct max of spaces

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const configutation = {
 };
 
 lambda(configutation, (input, output) => {
-  const parsedSpaces = Number(input.url.slice(1));
-  const spaces = isNaN(parsedSpaces) ? 2 : Math.max(parsedSpaces, 4);
+  const parsedSpaces = input.url !== '/' ? Number(input.url.slice(1)) : 2;
+  const spaces = isNaN(parsedSpaces) ? 2 : Math.min(parsedSpaces, 4);
   
   output.send(JSON.stringify(input.body, null, spaces));
 });
