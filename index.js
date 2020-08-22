cae1f734f5f7b403d23a2d6e43c0c59c787027c9
Darlan Alves <me@darlanalv.es>
2020-08-23 00:36:48 +0200

    feat: update to v2

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import { lambda, Format } from '@node-lambdas/core';
 
 const configutation = {
-  version: 1,
-  input: Format.Json,
-  output: Format.Text
+  version: 2,
+  actions: {
+    default: {
+      input: Format.Json,
+      output: Format.Text,
+      handler: (input, output) => {
+        const format = input.url.pathname.slice(1);
+        const formatAsNumber = Number(format);
+        const spaces = !isNaN(formatAsNumber) && formatAsNumber >= 0 && formatAsNumber <= 4 ? formatAsNumber : 2;
+        const char = format === 'TAB' ? '\t' : '';
+
+        output.send(JSON.stringify(input.body, null, char || spaces));
+      },
+    },
+  },
 };
 
-lambda(configutation, (input, output) => {
-  const format = input.url.slice(1);
-  const formatAsNumber = Number(format);
-  const spaces = !isNaN(formatAsNumber) && formatAsNumber >= 0 && formatAsNumber <= 4 ? formatAsNumber : 2;
-  const char = format === 'TAB' ? '\t' : '';
-
-  output.send(JSON.stringify(input.body, null, char || spaces));
-});
+lambda(configutation);
